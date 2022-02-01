6f56746f5ef54dfc87e024b118b504a5e928b198
Elvis Landi <elandi13@yahoo.com>
2022-02-01 13:21:01 -0500

    created Product component

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+import Product from './Product/Product';
+
+const products  = [
+    { id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: "https://media.cntraveler.com/photos/603e8e5eb3c7f5651f698239/16:9/w_2560%2Cc_limit/BestRunningSneakers-2021-Hoka-Lede.jpg"},
+    { id: 2, name: 'Macbook', description: 'Apple Macbook', price: '$10', image:'https://www.apple.com/v/macbook-pro-14-and-16/b/images/overview/hero/intro__ewz1ro7xs14y_large.jpg'}
+]
+
+
+const Products = () => {
+    return (
+
+        <main>
+        <Grid container justify="center" spacing={4}>
+            {products.map((product) => (
+                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <Product product={product} />
+                </Grid>
+            ))}
+        </Grid>
+
+    </main>
+    )   
+
+}
+
+export default Products;
