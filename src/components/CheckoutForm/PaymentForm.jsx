a71db6cf791b03ca2e48eedccbb51fe190946150
Elvis Landi <elandi13@yahoo.com>
2022-02-03 11:13:58 -0500

    wrote checkout summary logic

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Typography, Button, Divider} from '@material-ui/core';
+import { Elements, CardElement, ElementConsumer } from '@stripe/react-stripe-js';
+import { loadStrip } from '@stripe/stripe-js';
 
-export const PaymentForm = () => {
-  return <div> PaymentForm</div>;
+import Review from './Review'
+
+
+export const PaymentForm = ({checkoutToken}) => {
+  return ( 
+    <>
+        <Review checkoutToken={checkoutToken} />
+    
+    </>
+  )
 };
